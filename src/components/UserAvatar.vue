5dbeb9b38cc938a6ca39dbd943480de7cdd43314
GreiyD <dima.pidchenko@gmail.com>
2024-12-13 23:35:59 +0300

    Сделал прототип формы логина, с отправкой запроса на вход и выводом валидации

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,9 +1,18 @@
 <template>
+  <span v-if="nickname" class="fw-bold me-3">{{ nickname }}</span>
   <img src="https://github.com/mdo.png" alt="User Avatar" width="50" height="50" class="rounded-circle">
 </template>
 
 <script>
 export default {
   name: 'UserAvatar',
+  data() {
+    return {
+      nickname: null,
+    };
+  },
+  created() {
+    this.nickname = localStorage.getItem('nickname');
+  },
 };
 </script>
--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,18 +1,31 @@
 <template>
-  <form class="forms-sample">
+  <form class="forms-sample" @submit.prevent="handleLogin">
     <div class="mb-3">
-      <InputField id="email" label="Email" type="email" placeholder="Enter email"/>
+      <InputField
+          id="email"
+          v-model="formData.email"
+          label="Email"
+          type="email"
+          placeholder="Enter email"
+          :error="errors?.email?.[0] || ''"
+      />
     </div>
     <div class="mb-3">
-      <InputField id="password" label="Password" type="password" placeholder="Enter password"/>
+      <InputField
+          id="password"
+          v-model="formData.password"
+          label="Password"
+          type="password"
+          placeholder="Enter password"
+          :error="errors?.password?.[0] || ''"
+      />
     </div>
-
     <div class="row mt-4 mb-3">
       <div class="col-6">
-        <SubmitButton label="Submit"/>
+        <SubmitButton @click="handleLogin" label="Submit"/>
       </div>
       <div class="col-6 align-self-center text-secondary text-decoration-underline">
-        Forgot your password ?
+        Forgot your password?
       </div>
     </div>
   </form>
@@ -21,12 +34,58 @@
 <script>
 import InputField from '../InputField.vue';
 import SubmitButton from '../SubmitButton.vue';
+import {useToast} from "vue-toastification";
+import {loginUser} from '../../api/users'
 
 export default {
   name: 'LoginForm',
   components: {
     InputField,
     SubmitButton,
-  }
-}
+  },
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: '',
+      },
+      errors: {},
+    };
+  },
+  methods: {
+    parseErrors(errorArray) {
+      const parsed = {};
+      errorArray.forEach((error) => {
+        if (!parsed[error.field]) {
+          parsed[error.field] = [];
+        }
+        parsed[error.field].push(error.message);
+      });
+      return parsed;
+    },
+    async handleLogin() {
+      const toast = useToast();
+
+      try {
+        const response = await loginUser(this.formData);
+        const { name } = response.data.user;
+
+        localStorage.setItem('nickname', name);
+
+        toast.success('Login successful!');
+        this.$router.push('/');
+      } catch (error) {
+
+        if (error.response && error.response.status === 400) {
+          this.errors = error.response.data.errors || {};
+          this.errors = this.parseErrors(error.response.data.errors);
+
+          toast.error(error.response.data.message);
+        } else {
+          toast.error(error.response?.data?.message || 'Login failed. Please try again.');
+        }
+      }
+    },
+  },
+};
 </script>
